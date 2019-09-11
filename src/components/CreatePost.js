c73eabf49c9ea9477158034035d1423ee4bbb88e
pllenggg <plengna@gmail.com>
2019-09-11 16:53:45 +1000

    cloudinary widget is now working but fail to post

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,66 +1,96 @@
 import React, { Component } from 'react';
-import { Form, Button } from "react-bootstrap";
+import { Container, Form, Button } from "react-bootstrap";
+import NavBar from '../NavBar';
 import axios from 'axios';
 
 class CreatePost extends Component {
   constructor() {
     super();
-    this.state = { caption: '', post_image: '' };
-    this._handleSubmit = this._handleSubmit.bind(this);
+    this.state={
+      form: {
+        post_image: '',
+        caption: ''
+      }
+    }
+    this.uploadWidget = this.uploadWidget.bind(this);
     this._handleChange = this._handleChange.bind(this);
-  };
+    this._handleSubmit = this._handleSubmit.bind(this);
+    this.savePost = this.savePost.bind(this);
+  };//constructor ended here
 
+  uploadWidget() {
+    window.cloudinary.openUploadWidget({cloud_name:"dluw1enan", upload_preset:"pd78zj38"}, (err,result) => {
+      if(result) {
+        const image = result[0];
+        const newFormImg = {post_image: image.secure_url};
+        this.setState(({ form }) => {
+          return{
+            form: {
+              ...form,
+              ...newFormImg
+            }
+          }
+        })
+      }
+    })
+  } //uploadWidget
 
-
-  _handleChange(event) {
-    this.setState({
+  _handleChange(event){
+    const newFormData = {
       [event.target.name]: event.target.value
-    })
-  }
-  _handleSubmit(event) {
-    event.preventDefault();
-    const POSTS_API = 'http://localhost:3001/api/posts.json'
-    const token = "Bearer " + localStorage.getItem("jwt");
-    const data = { caption: this.state.caption, post_image: this.state.post_image }
-
-   axios({method: 'post', url: POSTS_API, headers: {'Authorization': token}, data: { data }
-    }).then((results) => {
-      console.log(results.data)
-    })
-  }
-  
-
-  uploadWidget = () => {
-    window.cloudinary.openUploadWidget({ cloud_name: 'dluw1enan', upload_preset: 'pd78zj38' },
-      (error, result) => {
-        if (result) {
-          const data = result[0];
-          console.log(data, "successfully opened")
-        } else {
-          console.log(error);
+    }
+    this.setState(({ form }) => {
+      return {
+        form: {
+          ...form,
+          ...newFormData
         }
-      })
-  }
+      }
+    })
+  }//handlechange
 
+  savePost(post) {
+    const url = 'http://localhost:3001/api/posts.json'
+    let token = "Bearer "+localStorage.getItem('jwt');
+    axios({method:"post", url: url, header:{'Authorization': token}, data: post}).then(()=> {
+      this.history.push(-1);
+    });
+  }//savePost
+
+  _handleSubmit(e) {
+    e.preventDefault();
+    const data = {
+      caption: form.caption,
+      post_image: form.post_image
+    }
+    const { form } = this.state
+    this.props.onSubmit(data);
+
+  }//handlesubmit
   render(){
-    return (
-      <div >
-        <Form onSubmit={this._handleSubmit} >
-          <Form.Group controlId="caption">
-            <Form.Label>Caption:</Form.Label>
-            <Form.Control placeholder="Write your caption" name="caption" value={this.state.caption} onChange={this._handleChange} required maxLength="100" />
-          </Form.Group>
+    const { form } = this.state;
+    return(
+      <div>
+        <NavBar/>
+        <Container>
+          <Form onSubmit= {this.savePost}>
+            <Form.Group controlId="exampleForm.ControlInput1">
+              <Form.Label>Image</Form.Label>
+              <Form.Control name="post_image" value={form.post_image} type="text" placeholder="img URL here.." onChange={this._handleChange}/>
+              <Button onClick={this.uploadWidget}>Upload</Button>
+            </Form.Group>
 
-          <Form.Group controlId="image">
-            <Form.Label>Image</Form.Label>
-            <Form.Control placeholder="Add Image URL ..." type="text" name="post_image" value={this.state.post_image} onChange={this._handleChange} readOnly="true"></Form.Control>
-            <Button variant="info" onClick={this.uploadWidget.bind(this)}>Select Image</Button>
-            
-          </Form.Group>
-          <Button variant="info" type="submit">Share</Button>
-        </Form>
-      </div >
+            <Form.Group controlId="exampleForm.ControlInput1">
+              <Form.Label>Caption</Form.Label>
+              <Form.Control type="text" name="caption" value={form.caption} onChange={this._handleChange} placeholder="what's on today?" />
+            </Form.Group>
+
+            <Button type="submit">Share</Button>
+
+          </Form>
+        </Container>
+      </div>
     )
   }
-}
+}// class component end here
 export default CreatePost;
