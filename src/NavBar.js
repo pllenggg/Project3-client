e2ae85e90f9bf5e0b893c6ab658e0622b75df6ad
pllenggg <plengna@gmail.com>
2019-09-12 16:36:58 +1000

    added signin and out to nav bar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,38 +1,35 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { Nav, Navbar} from "react-bootstrap";
 
 
-class Navigation extends Component {
-  render() {
-    return (
-     <div>
-
-       
-         <Navbar bg="light" expand="lg">
-         <Navbar.Brand href="#/profile">
-           <img src="https://res.cloudinary.com/dluw1enan/image/upload/c_scale,w_30/v1567989092/project3/cat-icon_kpljby.png" alt="meow logo"/>
-         </Navbar.Brand>
-         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-           <Nav className="mr-auto">
-             <Nav.Link href="#"><strong>Meowstagram</strong></Nav.Link>
-             <Nav.Link href="#/profile">Profile</Nav.Link>
-             <Nav.Link href="#/search">
-               <img src="https://res.cloudinary.com/dluw1enan/image/upload/c_scale,w_30/v1568179422/project3/search_icon_crfdts.png" alt="search-icon"/>
-             </Nav.Link>
-             <Nav.Link href="#/add">
-             <img src="https://res.cloudinary.com/dluw1enan/image/upload/c_scale,w_30/v1568179420/project3/small_plus_icon_ojgk9c.png" alt="add icon"/>
-             </Nav.Link>
-            </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-     </div>
-
+const Navigation = ()  => {
+    return(
+      <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#/profile">
+        <img src="https://res.cloudinary.com/dluw1enan/image/upload/c_scale,w_30/v1567989092/project3/cat-icon_kpljby.png" alt="meow logo"/>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+       <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#"><strong>Meowstagram</strong></Nav.Link>
+          <Nav.Link href="#/profile">Profile</Nav.Link>
+          <Nav.Link href="#/search">
+            <img src="https://res.cloudinary.com/dluw1enan/image/upload/c_scale,w_30/v1568179422/project3/search_icon_crfdts.png" alt="search-icon"/>
+          </Nav.Link>
+          <Nav.Link href="#/add">
+          <img src="https://res.cloudinary.com/dluw1enan/image/upload/c_scale,w_30/v1568179420/project3/small_plus_icon_ojgk9c.png" alt="add icon"/>
+          </Nav.Link>
+    {
+        localStorage.getItem("jwt") ?
+          <Nav.Link href="#/signout">Sign out</Nav.Link>
+        :
+          <Nav.Link href="/">Log In</Nav.Link>
+      }
+         </Nav>
+     </Navbar.Collapse>
+   </Navbar>
     )
   }
-}
-  
-    
 
 export default Navigation;
 
