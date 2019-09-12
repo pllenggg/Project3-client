3f6ac07b62410c4b7324ba0c5cd435c3361acb1c
pllenggg <plengna@gmail.com>
2019-09-12 17:30:48 +1000

    logout bugs fixed

--- src/components/Signout.js
+++ src/components/Signout.js
@@ -7,6 +7,8 @@ class SignOut extends Component {
 
   render () {
     localStorage.removeItem('jwt');
+    localStorage.removeItem('user_id');
+    localStorage.setItem ("user_name");
     this.props.history.push("/");
     window.location.reload();
     return;
